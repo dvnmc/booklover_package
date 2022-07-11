f474346cd647b99ce565d1e7960174e1883c6ca0
Devin McDonald <devinmcdonald@MacBook-Pro.local>
2022-07-11 11:14:29 -0400

    Set up file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Booklover09',
+    version='1.0.0',
+    url='https://github.com/mypackage.git',
+    author='Devin McDonald',
+    author_email='djm6cz@virginia.edu',
+    description='Package containing files used in M09 HW',
+    packages=find_packages(),    
+    install_requires=['numpy', 'pandas', 'unittest'],
+)
